8e4c2f6f33f8f2b5a316b7a7b734db99938a69b9
mromeroruiz <mromeroruiz.business@gmail.com>
2019-03-26 11:19:30 -0400

    testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ switch (window.location.hostname) {
     // this is the deployed angular application
     case 'ang-milestones.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://milestones1150.azurewebsites.net'
+        APIURL = 'milestones1150.azurewebsites.net'
         break;
     default:
         // this is the local host name of your API
