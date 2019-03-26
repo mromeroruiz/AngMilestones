3eb1f38ac2992623b50051e41d877c232ba339c1
lkucera94 <lekucera94@gmail.com>
2019-03-26 09:17:25 -0400

    styling

--- src/app/components/kid/kid-create/kid-create.component.ts
+++ src/app/components/kid/kid-create/kid-create.component.ts
@@ -3,14 +3,19 @@ import { KidsService } from '../../../services/kids.service';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
+
+
+
 @Component({
   selector: 'app-kid-create',
   templateUrl: './kid-create.component.html',
   styleUrls: ['./kid-create.component.css']
 })
+
 export class KidCreateComponent implements OnInit {
 
   kidForm: FormGroup;
+
 
   constructor(private _kidService: KidsService, private _form: FormBuilder, private _router: Router) { 
     this.createForm();
