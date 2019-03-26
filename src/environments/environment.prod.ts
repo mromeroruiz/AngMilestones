123376db6959009907e9ef0d0a1fd1742e1a02b0
mromeroruiz <mromeroruiz.business@gmail.com>
2019-03-26 13:23:24 -0400

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export let APIURL = '';
 
 switch (window.location.hostname) {
     // this is the deployed angular application
-    case 'https://ang-milestones.herokuapp.com':
+    case 'ang-milestones.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://milestones1150.azurewebsites.net'
         break;
